ea8d1ae574ee34abe801908f2c1639881fe1380f
Kushal <kushal-shrestha@hotmail.com>
2020-10-08 19:08:30 +0545

    only windows builds

--- create.sh
+++
@@ -1,9 +0,0 @@
-UNITY_VERSION=2020.1.7f1
-docker run -it --rm \
--e "UNITY_USERNAME=kushal-shrestha@hotmail.com" \
--e "UNITY_PASSWORD=Kushal9841508775" \
--e "TEST_PLATFORM=linux" \
--e "WORKDIR=/root/project" \
--v "$(pwd):/root/project" \
-gableroux/unity3d:$UNITY_VERSION \
-bash
