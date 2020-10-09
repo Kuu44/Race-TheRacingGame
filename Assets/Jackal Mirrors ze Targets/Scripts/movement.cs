1bd0d6b8a971a5b4fc3a2e12b6eb4f7ff0c7a947
Kushal <kushal-shrestha@hotmail.com>
2020-10-09 15:05:01 +0545

    Testing out some networking

---
+++ Assets/Jackal Mirrors ze Targets/Scripts/movement.cs
@@ -0,0 +1,25 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class movement : NetworkBehaviour
+{
+    [SerializeField]
+    [Range(1f, 20f)]
+    private float speed;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+        if (isLocalPlayer)
+            transform.Translate(inputX * speed * Time.deltaTime, inputY * speed * Time.deltaTime, 0);
+    }
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+    }
+}
