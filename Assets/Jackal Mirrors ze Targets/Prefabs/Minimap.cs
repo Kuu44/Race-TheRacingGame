1b07e4472493795a4ff9bb7d923a1ef601dcdcca
Pramish Aryal <60599905+Pramish-Aryal@users.noreply.github.com>
2020-10-23 14:07:24 +0545

    minimap v0.01
    even more bonkers

---
+++ Assets/Jackal Mirrors ze Targets/Prefabs/Minimap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    float cameraDistanceAbove = 100f;
+    [Range(0, 1)]
+    float followSpeed = .2f;
+
+    Camera minimapCamera;
+
+    void Start()
+    {
+        minimapCamera = GameObject.FindGameObjectWithTag("Minimap Camera").GetComponent<Camera>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movetarget = transform.position + Vector3.up * cameraDistanceAbove;
+
+        minimapCamera.transform.position = Vector3.Lerp(minimapCamera.transform.position, movetarget, followSpeed);
+    }
+}
